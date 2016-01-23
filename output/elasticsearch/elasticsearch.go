d0372805d9237db1f14cf1c223e0c149fe4f6c2c
Stan Hu <stanhu@gmail.com>
2016-01-23 08:49:45 +0000

    Don't loop forever waiting for data

--- output/elasticsearch/elasticsearch.go
+++ output/elasticsearch/elasticsearch.go
@@ -111,17 +111,15 @@ func (e *ESServer) Init(config json.RawMessage, b buffer.Sender) error {
 }
 
 func readInputChannel(idx *Indexer, receiveChan chan *buffer.Event) {
-	for {
-		// Drain the channel only if we have room
-		if idx.bulkService.NumberOfActions() < esSendBuffer {
-			select {
-			case ev := <-receiveChan:
-				idx.index(ev)
-			}
-		} else {
-			log.Printf("Internal Elasticsearch buffer is full, waiting")
-			time.Sleep(1 * time.Second)
+	// Drain the channel only if we have room
+	if idx.bulkService.NumberOfActions() < esSendBuffer {
+		select {
+		case ev := <-receiveChan:
+			idx.index(ev)
 		}
+	} else {
+		log.Printf("Internal Elasticsearch buffer is full, waiting")
+		time.Sleep(1 * time.Second)
 	}
 }
 

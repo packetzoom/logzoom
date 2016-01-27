23b75fb0e64b124d04bb71e4bad8d06f0990001e
Stan Hu <stanhu@gmail.com>
2016-01-27 02:19:05 +0000

    Add support for CPU and memory profiling

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 
 	_ "github.com/packetzoom/logslammer/input/lumberjack"
@@ -13,13 +14,21 @@ import (
 	_ "github.com/packetzoom/logslammer/output/tcp"
 	_ "github.com/packetzoom/logslammer/output/websocket"
 	"github.com/packetzoom/logslammer/server"
+	"net/http"
+	_ "net/http/pprof"
+	"runtime"
+	"runtime/pprof"
 )
 
 var (
-	config string
+	config                           string
+	memprofile, cpuprofile, httpprof *string
 )
 
 func init() {
+	memprofile = flag.String("memprofile", "", "Write memory profile to this file")
+	cpuprofile = flag.String("cpuprofile", "", "Write cpu profile to file")
+	httpprof = flag.String("httpprof", "", "Start pprof http server")
 	flag.StringVar(&config, "config", "", "Path to the config file")
 	flag.Parse()
 
@@ -30,7 +39,61 @@ func init() {
 	}
 }
 
+func writeHeapProfile(filename string) {
+	f, err := os.Create(filename)
+	if err != nil {
+		log.Printf("Failed creating file %s: %s\n", filename, err)
+		return
+	}
+	pprof.WriteHeapProfile(f)
+	f.Close()
+
+	log.Printf("Created memory profile file %s.\n", filename)
+}
+
+func Cleanup() {
+	if *cpuprofile != "" {
+		pprof.StopCPUProfile()
+	}
+
+	if *memprofile != "" {
+		runtime.GC()
+
+		writeHeapProfile(*memprofile)
+
+		debugMemStats()
+	}
+}
+
+func debugMemStats() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	log.Printf("Memory stats: In use: %d Total (even if freed): %d System: %d\n",
+		m.Alloc, m.TotalAlloc, m.Sys)
+}
+
+func BeforeRun() {
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+	}
+
+	if *httpprof != "" {
+		go func() {
+			log.Println("start pprof endpoint")
+			log.Printf("finished pprof endpoint: %v\n", http.ListenAndServe(*httpprof, nil))
+		}()
+	}
+}
+
 func main() {
+	defer Cleanup()
+
+	BeforeRun()
+
 	srv, err := server.New(config)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
